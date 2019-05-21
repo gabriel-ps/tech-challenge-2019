7b1d35c58258d46ba67910e3be50d3096feb6ada
Gabriel Pereira da Silva <gabriel.gps@outlook.com>
2019-05-21 17:15:43 -0300

    Test database configured to be an in memory SQLite database. Helper functions added to make tests more readable. Test example files removed.

---
+++ tests/utilities/functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Simple helper functions to make tests more readable
+ */
+
+function create($class, $attributes = [])
+{
+    return factory($class)->create($attributes);
+}
+
+function make($class, $attributes = [])
+{
+    return factory($class)->make($attributes);
+}
